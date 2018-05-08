255935a79f0813697bd3501a53562ded101b6c9e
Mayar Ali <mayarali@wlan247-180.extern.hu-berlin.de>
2018-05-08 10:31:10 +0200

    modified the directory values to tuples

--- city_location.py
+++ city_location.py
@@ -14,8 +14,14 @@ def get_coordinates(city):
 def map_cities(cities):
     dic = {}
     for city in cities:
+        time.sleep(5)
         print(city)
-        dic[city] = get_coordinates(city)
+        coordinates = get_coordinates(city)
+        dic[city] = (coordinates['lat'], coordinates['lng'])
         print()
         time.sleep(10)
+        print(dic[city])
     return dic
+
+cities=['BERLIN', 'HAMBURG', 'MUNICH', 'FRANKFURT', 'COLOGNE']
+print(map_cities(cities))
