a9f558b7d87a51cfcba4145abd0fb211e88638fe
Daniel Vargas <daniel.vhya@gmail.com>
2018-05-08 10:26:34 +0200

    WS-0001 Creation of a script that stores the weather forecast as a file, using the coordinates of cities

---
+++ constants.py
@@ -0,0 +1,12 @@
+KEY = "ba3288780ec658cc"
+BASE_URL = "http://api.wunderground.com/api/"+ KEY +"/forecast10day/q/"
+FILENAME = "simple_forecast.json"
+CITIES = ["Berlin", "Hamburg", "Munich", "Cologne", "Frankfurt"]
+
+#Coordinates
+coordinates = {"Berlin":   (52.5200, 13.4050),
+               "Hamburg":  (53.5511,  9.9937),
+               "Munich":   (48.1351, 11.5820),
+               "Cologne":  (50.9375,  6.9603),
+               "Frankfurt":(50.1109,  8.6821),
+}
